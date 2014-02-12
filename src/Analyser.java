fceb51ba83c890b5887e53dea1c350868c6bc033
craigpullar <craigmichaelpullar@gmail.com>
2014-02-12 18:04:33 +0000

    LingPipe Added/Analyser update

--- src/Analyser.java
+++ src/Analyser.java
@@ -8,6 +8,8 @@ public class Analyser {
 	private Analysis analysis;
 	private Sentiment sentiment;
 	private ArrayList<Sentiment> sentiments;
+	private Location location;
+	private ArrayList<Location> locations;
 	
 	//--------------------\\
 	//--[[CONSTRUCTORS]]--\\
@@ -21,6 +23,11 @@ public class Analyser {
 	public ArrayList<Sentiment> sentimentAnalysis(ArrayList<Tweet> tweets) {
 		
 		return this.sentiments;
+	}
+	
+	public ArrayList<Location> locationAnalysis(ArrayList<Tweet> tweets) {
+		
+		return this.locations;
 	}
 
 	//-------------------------\\
@@ -48,5 +55,21 @@ public class Analyser {
 
 	public void setSentiments(ArrayList<Sentiment> sentiments) {
 		this.sentiments = sentiments;
+	}
+	
+	public Location getLocation() {
+		return location;
+	}
+	
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+	
+	public ArrayList<Location> getLocations() {
+		return locations;
+	}
+	
+	public void setLocations(ArrayList<Location> locations) {
+		this.locations = locations;
 	}
 }
